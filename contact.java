faa90d7830513a5a0d1752d98a6dd9d1977678fc
varshinielectronics <varshinivarshini87458@gmail.com>
2026-05-21 13:03:41 +0530

    Add files via upload

---
+++ contact.java
@@ -0,0 +1,77 @@
+package com.contactapp.model;
+
+public class contact {
+
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    // Default Constructor
+    public contact() {
+    }
+
+    // Parameterized Constructor
+    public contact(int id, String name, String email, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    // Getter and Setter Methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // toString Method
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
